bb18f0c18970847d207da565de8b109c06db3b0b
Camilo-H <camiload@unicauca.edu.co>
2024-08-10 18:26:48 -0500

    clonación de proyecto

---
+++ src/main/java/co/edu/unicauca/proyecto/estudiantes/dominio/model/Alumno.java
@@ -0,0 +1,13 @@
+package co.edu.unicauca.proyecto.estudiantes.dominio.model;
+
+import co.edu.unicauca.proyecto.models.enums.TipoAlumno;
+import lombok.*;
+
+@Data
+public class Alumno {
+     private String codigo;
+    //private Facultad facultad;
+    private TipoAlumno tipo;
+    //private List<Inscripcion> inscripciones;
+   // private List<Clase> clases;
+}
