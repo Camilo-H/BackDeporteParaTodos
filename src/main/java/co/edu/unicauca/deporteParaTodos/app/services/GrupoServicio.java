4d55d43806751b97d197f12c8ebe5c7b51ebad42
edynson muñoz jimenez <edinsonmjim@unicauca.edu.co>
2024-08-20 20:42:01 -0500

    prueba flujo de datos para grupos, exito en la creacion de la llave compuesta usando class id

---
+++ src/main/java/co/edu/unicauca/deporteParaTodos/app/services/GrupoServicio.java
@@ -0,0 +1,22 @@
+package co.edu.unicauca.deporteParaTodos.app.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import co.edu.unicauca.deporteParaTodos.core.domain.models.GrupoEntidad;
+import co.edu.unicauca.deporteParaTodos.core.domain.ports.inbound.IGrupoServicio;
+import co.edu.unicauca.deporteParaTodos.core.domain.ports.outbound.database.IGrupoRepositorio;
+
+@Service
+public class GrupoServicio implements IGrupoServicio{
+    
+    @Autowired
+    private IGrupoRepositorio repoGrupo;
+
+    @Override
+    public Iterable<GrupoEntidad> obtenerGrupos() {
+        return repoGrupo.findAll();
+    }
+
+
+}
