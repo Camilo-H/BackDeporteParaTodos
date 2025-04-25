c29e9329137b8d6d710d1bae103be854aa57a27c
Edynson MJ <edinsonmjim@unicauca.edu.co>
2025-04-25 16:18:06 -0500

    endpoint para stream de datos imagen

--- src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/ImagenRest.java
+++ src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/ImagenRest.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import ch.qos.logback.core.util.ContentTypeUtil;
 import co.edu.unicauca.deporteParaTodos.dominio.modelo.Imagen;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTO.v2DTO.V2ImagenDTO;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.entidades.ImagenEntidad;
@@ -51,7 +53,7 @@ public class ImagenRest {
         return new ResponseEntity<V2ImagenDTO>(respuestaDto,HttpStatus.CREATED);
     }
 
-    @GetMapping("imagen")
+    @GetMapping("/imagen")
     public ResponseEntity<V2ImagenDTO> getObtenerImagen(@RequestParam int idImagen) {
         Optional<ImagenEntidad> opcional = repositorio.findById(idImagen);
         if(opcional.isPresent()){
@@ -63,6 +65,24 @@ public class ImagenRest {
             return new ResponseEntity<>(dto,HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+    
+    @GetMapping("/imagenStream")
+    public ResponseEntity<byte[]> getMethodName(@RequestParam int idImagen) {
+        Optional<ImagenEntidad> op;
+        op = repositorio.findById(idImagen);
+        if(op.isEmpty() || op.get().getDatos()==null){
+            return ResponseEntity.notFound().build();
+        }
+        byte[] datos = op.get().getDatos();
+        String tipoMime = op.get().getTipoArchivo();
+        if(tipoMime==null || tipoMime.isEmpty()){
+            tipoMime = "image/jpeg";
+        }
+        MediaType mediaType = MediaType.IMAGE_JPEG;
+        mediaType = MediaType.parseMediaType(tipoMime);
+
+        return ResponseEntity.ok().contentType(mediaType).body(datos);
     }
     
     
