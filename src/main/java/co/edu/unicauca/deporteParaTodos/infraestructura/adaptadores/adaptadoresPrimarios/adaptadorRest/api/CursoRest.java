afe3c7fde0b14882328c92eba76e50073137444e
Edynson MJ <edinsonmjim@unicauca.edu.co>
2025-04-27 02:59:32 -0500

    agregar curso remoto, modificacion a retorno para obtener cursos

--- src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/CursoRest.java
+++ src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/CursoRest.java
@@ -1,8 +1,13 @@
 package co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.api;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.export.dynatrace.DynatraceProperties.V2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,10 +17,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTO.comunes.CursoDTO;
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTO.v2DTO.V2CursoDTO;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.entidades.CategoriaCursoEntidad;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.entidades.CursoEntidad;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.repositorios.ICategoriaCursoRepositorio;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.repositorios.ICursoRepositorio;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("api/v2")
@@ -31,13 +40,36 @@ public class CursoRest {
     }
 
     @GetMapping("/cursosbycategoria")
-    public Iterable<CursoEntidad> obtenerCursosPorCategoria(@RequestParam String prmCategoria){
-        return repositorio.findByCategoriaCursoAndEliminado(prmCategoria, 0);
+    public ResponseEntity<List<V2CursoDTO>> obtenerCursosPorCategoria(@RequestParam String prmCategoria){
+        List<CursoEntidad> entidades = repositorio.findByCategoriaCursoAndEliminado(prmCategoria, 0);
+        List<V2CursoDTO> dtos = new ArrayList<>();
+        for(CursoEntidad entidad : entidades){
+            V2CursoDTO dto = V2CursoDTO.fromEntity(entidad);
+            dtos.add(dto);
+        }
+        return new ResponseEntity<>(dtos,HttpStatus.OK);
     }
 
     @GetMapping("/curso")
     public CursoEntidad obtenerCurso(@RequestParam String prmCategoria, @RequestParam String prmCurso) {
         return repositorio.findByCategoriaCursoAndNombre(prmCategoria, prmCurso);
     }
+
+    @PostMapping("/curso")
+    public ResponseEntity<V2CursoDTO> postAgregarCurso(@RequestBody V2CursoDTO dto) {
+        CursoEntidad entidad = new CursoEntidad(
+            dto.getNombre(),
+            dto.getDeporte(),
+            dto.getCategoriaCurso(),
+            dto.getDescripcion(),
+            dto.getIdImagen(),
+            0
+        );
+        CursoEntidad entidadGuardada=null;
+        entidadGuardada = repositorio.save(entidad);
+        V2CursoDTO retorno = V2CursoDTO.fromEntity(entidadGuardada);
+        return new ResponseEntity<>(retorno,HttpStatus.CREATED);
+    }
+    
     
 }
