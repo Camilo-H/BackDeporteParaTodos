8971ceb2b302897077e9b582eba0f41e3bffc7d5
Edynson MJ <edinsonmjim@unicauca.edu.co>
2025-05-07 01:51:06 -0500

    endpoint insertar grupo

--- src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/GrupoRest.java
+++ src/main/java/co/edu/unicauca/deporteParaTodos/infraestructura/adaptadores/adaptadoresPrimarios/adaptadorRest/api/GrupoRest.java
@@ -1,9 +1,11 @@
 package co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.api;
 
+import java.sql.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -12,8 +14,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTO.v2DTO.V2GrupoDTO;
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTO.v2DTO.V2InstructorDTO;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.entidades.GrupoEntidad;
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.repositorios.ICategoriaCursoRepositorio;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresSecundarios.persistenciaSQL.repositorios.IGrupoRepositorio;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("api/v2")
@@ -22,6 +30,9 @@ import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresS
 public class GrupoRest {
     @Autowired
     private IGrupoRepositorio repositorio;
+
+    @Autowired 
+    private ICategoriaCursoRepositorio repoCategoria;
 
     @GetMapping("/grupos")
     public Iterable<GrupoEntidad> obtenerCursos(){
@@ -48,5 +59,31 @@ public class GrupoRest {
         List<GrupoEntidad> resultado = repositorio.findByIdInstructor(idInstructor);
         return new ResponseEntity<>(resultado,HttpStatus.OK);
     }
+
+    @PostMapping("/grupo")
+    public ResponseEntity<GrupoEntidad> postGrupo(@RequestBody V2GrupoDTO dto) {
+        List<GrupoEntidad> entidades = repositorio.findByCategoriaAndCursoAndEliminado(dto.getCat_titulo(), dto.getCur_nombre(), 0);
+        int pivote = 0;
+        for (GrupoEntidad grupoEntidad : entidades) {
+            int temporal = grupoEntidad.getIterable();
+            if(pivote < temporal){
+                pivote = temporal;
+            }
+        }
+        GrupoEntidad entidad = new GrupoEntidad();
+        entidad.setCategoria(dto.getCat_titulo());
+        entidad.setCurso(dto.getCur_nombre());
+        entidad.setAnio(dto.getAnio());
+        entidad.setIterable(pivote+1);
+        entidad.setCupos(dto.getCupos());
+        entidad.setEliminado(0);
+        entidad.setFechaCreacion(Date.valueOf(dto.getFechaCreacion()));
+        entidad.setIdInstructor(dto.getIdInstructor());
+        entidad.setImagenGrupo(dto.getImagen());
+        
+        repositorio.save(entidad);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+    
     
 }
