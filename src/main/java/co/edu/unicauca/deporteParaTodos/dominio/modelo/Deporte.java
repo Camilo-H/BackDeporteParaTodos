8758e5d70adc04657772e8a01d90aaa825772ece
Camilo-H <camiload@unicauca.edu.co>
2026-03-24 22:18:37 -0500

    Implementación del método post para los  Deportes

--- src/main/java/co/edu/unicauca/deporteParaTodos/dominio/modelo/Deporte.java
+++ src/main/java/co/edu/unicauca/deporteParaTodos/dominio/modelo/Deporte.java
@@ -1,5 +1,6 @@
 package co.edu.unicauca.deporteParaTodos.dominio.modelo;
 
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTOs.DeporteDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +11,16 @@ import lombok.Setter;
 @Setter
 @Getter
 public class Deporte {
+    
     private String nombre;
+
+    public static Deporte fabricarDeDto(DeporteDto dto){
+        try{
+            Deporte fabricado = new Deporte();
+            fabricado.setNombre(dto.getNombre());
+            return fabricado;
+        }catch(Exception e){
+            return null;
+        }
+    }
 }
