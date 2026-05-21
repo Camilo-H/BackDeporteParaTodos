47f0f8cf8bb47501c1d6ff4c73ed0c32b79a0877
Jua-n10 <sebastianpisso00272@gmail.com>
2026-05-21 10:53:37 -0500

    agregando end point para registrar instructor desde el admin

--- src/main/java/co/edu/unicauca/deporteParaTodos/aplicacion/puertos/puertosEntrada/IInstructorServicio.java
+++ src/main/java/co/edu/unicauca/deporteParaTodos/aplicacion/puertos/puertosEntrada/IInstructorServicio.java
@@ -3,6 +3,7 @@ package co.edu.unicauca.deporteParaTodos.aplicacion.puertos.puertosEntrada;
 import java.util.List;
 import co.edu.unicauca.deporteParaTodos.dominio.modelo.Instructor;
 import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTOs.InstructorDto;
+import co.edu.unicauca.deporteParaTodos.infraestructura.adaptadores.adaptadoresPrimarios.adaptadorRest.DTOs.PerfilDto;
 
 public interface IInstructorServicio {
 
@@ -15,5 +16,7 @@ public interface IInstructorServicio {
     public Instructor actualizarInstructor(String instructorId, Instructor datosInstructor);
 
     public Instructor eliminarInstructor(String instructorId);
+
+    public InstructorDto registrarInstructor(PerfilDto perfilDto);
 
 }
